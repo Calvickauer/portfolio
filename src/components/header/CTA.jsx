d35220961405e85642acf64414014a392e6c4946
calvickauer <calvickauer@gmail.com>
2023-01-05 20:17:02 -0800

    change cv download file name

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Calvin_Moldenhauer_Resume from '../../assets/Resume/Resume.pdf'
+import Resume from '../../assets/Resume/Calvin_Moldenhauer_Resume.pdf'
 // import CV from '../../assets/vc.pdf' # need to make this still
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Calvin_Moldenhauer_Resume} download className='btn'> Download CV </a>
+        <a href={Resume} download className='btn'> Download CV </a>
         <a href='#contact' className='btn btn-primary'> Let's Talk </a>
     </div>
   )
